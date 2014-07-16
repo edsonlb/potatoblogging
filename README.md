4bb85acb51bb2a751c07048e4e559bab2af82805
edsonlb <ti_edson@yahoo.com.br>
2014-07-16 12:36:13 -0300

    Readme up

---
+++ README.md
@@ -0,0 +1,9 @@
+Potato Blogging Tool
+=========
+<br />
+<p align="center"><img src="http://edsonlopes.com/arquivos/potato.jpg"></p>
+<br />
+Project developed for Potato London
+<br />
+
+https://github.com/edsonlb
--- potatoblogging/settings.py
+++ potatoblogging/settings.py
@@ -10,14 +10,15 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
+from settings_secret import * 
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.6/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'n2$kjp$92(o)#4lasvzz2jg0q2$nekdb)mie4rkx4p^4bnnw5&'
+# SECURITY WARNING: keep the secret key used in production secret! See my settings_secret configuration.
+SECRET_KEY = SECRET_KEY_SETTINGS
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
