5d490980b01c7e912935cb553f9b9a56cbce5d79
edsonlb <ti_edson@yahoo.com.br>
2014-07-17 01:44:22 -0300

    Porting from Django 1.6 to 1.5 for GAE

--- potatoblogging/urls.py
+++ potatoblogging/urls.py
@@ -1,9 +1,12 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^media(.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+    #url(r'^static(.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
     url(r'^$', 'blog.views.index'),
     url(r'^blog/', include('blog.urls')),
     url(r'^myadmin/', include('myadmin.urls')),
